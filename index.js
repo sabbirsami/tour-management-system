3e99d897128960e4a663d5942dd4f560c989bdea
SabbirSami <smd71430@gmail.com>
2022-09-21 12:09:13 +0600

    get api ready

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const app = express();
 require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+const tourRoute = require("./routes/tour.route");
 
 // MIDDLEWARE
 app.use(cors);
 app.use(express.json());
+
+// FOR ALL TOUR API
+app.use("/api/tour", tourRoute);
 
 app.get("/", (req, res) => {
     res.send("Tour Management System");
